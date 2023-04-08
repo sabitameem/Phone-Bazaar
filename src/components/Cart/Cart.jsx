441f076caee76a169cbfe5f87d9f93f3c71229c4
sabitameem <sabitameem578@gmail.com>
2023-04-09 01:11:18 +0530

    shop section fixed

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,10 +5,10 @@ import { Toaster } from 'react-hot-toast';
 const Cart = ({cart,handleRemoveFromCart}) => {
     return (
         <div className='sticky top-0 z-50'>
-            <h2 className='text-2xl font-bold text-center text-white bg-orange-500 rounded-lg p-2 mb-3'>Total Order Summary : {cart.length}</h2>
+            <h2 className='text-2xl font-bold text-center text-white bg-orange-500 rounded-lg p-1 mb-3'>Order Summary : {cart.length}</h2>
             {
                 cart.map(ph =>
-                    <p className='text-xl font-semibold inline-flex items-center mt-1'
+                    <p className='text-lg font-semibold inline-flex items-center mt-1'
                     key={ph.id}
                     >{ph.name}
                     <button onClick={()=> handleRemoveFromCart(ph.id)} className='mx-3'><TrashIcon className= "h-6 w-6 text-blue-500" /></button>
